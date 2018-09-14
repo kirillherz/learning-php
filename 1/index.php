d6270cb7c3fef1cb6ce3e7df861b343b33c70de8
Kirill <kirill.herz@gmail.com>
2018-09-14 19:48:21 +0300

    Добавлена работа с Twig

--- 1/index.php
+++ 1/index.php
@@ -1,3 +1,4 @@
+<?php
 
 class ServiceTwig {
 
@@ -13,6 +14,8 @@ class ServiceTwig {
 
 }
 
+$uri = $_SERVER['REQUEST_URI'];
+$array = array();
 array_push($array, array(
     'pattern' => '/^\/def\/save\/?$/',
     'func' => function() {
@@ -20,3 +23,25 @@ array_push($array, array(
         echo $twig->render('index.html', array('msg' => "hello"));
     }));
     
+array_push($array, array(
+    'pattern' => '/^\/def\/del\/(\d+)\/(\d+)$/',
+    'func' => function($number, $number2) {
+        echo $number;
+        echo '<br>';
+        echo $number2;
+    }));
+
+
+
+$flag = true;
+foreach ($array as $key => $value) {
+    if (preg_match($value['pattern'], $uri, $matches)) {
+        array_shift($matches);
+        $value['func'](...$matches);
+        $flag = false;
+        break;
+    }
+}
+if ($flag) {
+    echo 'error';
+}
