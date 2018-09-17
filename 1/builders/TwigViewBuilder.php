f10cae737b3572d6ac4e5108acbd88715047fad1
Kirill <kirill.herz@gmail.com>
2018-09-18 00:00:20 +0300

    Перемещен класс
    TwigViewAutoTemplateAndAutoLoadPath перемещен в TwigViewBuilder

--- 1/builders/TwigViewBuilder.php
+++ 1/builders/TwigViewBuilder.php
@@ -1,3 +1,5 @@
+<?php
+
 class TwigViewBuilder {
 
     protected $twigView;
@@ -13,6 +15,32 @@ class TwigViewBuilder {
 
     public function createTwigView() {
         $this->twigView = new $this->className;
+    }
+
+}
+
+class TwigViewAutoTemplateAndAutoLoadPath extends TwigViewBuilder {
+
+    public function buildAutoReload() {
+        $this->twigView->setAutoReload(true);
+    }
+
+    public function buildTemplatePath() {
+
+        $this->twigView->setTemplatesPath('templates');
+    }
+    
+    public function buildAutoloadPath() {
+
+        $this->twigView->setAutoloadPath('./vendor/autoload.php');
+    }
+
+    public function buildCache() {
+        $this->twigView->setCache('compilation_cache');
+    }
+
+    public function BuildTwigEnvironment() {
+        $this->twigView->setTwigEnvironment();
     }
 
 }
