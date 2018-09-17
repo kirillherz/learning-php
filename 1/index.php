7c630152b9abcb7ebc9cb7fe327456a31fe0dbd8
Kirill <kirill.herz@gmail.com>
2018-09-17 17:12:03 +0300

    Добавлен класс TwigBuilder

--- 1/index.php
+++ 1/index.php
@@ -111,6 +111,25 @@ class HelloView extends TwigView {
 $uri = $_SERVER['REQUEST_URI'];
 $array = array();
 
+class TwigViewBuilder {
+
+    protected $twigView;
+    protected $className;
+
+    public function __construct(string $className) {
+        $this->className = $className;
+    }
+
+    public function getTwigView(): TwigView {
+        return $this->twigView;
+    }
+
+    public function createTwigView() {
+        $this->twigView = new $this->className;
+    }
+
+}
+
 
 array_push($array, array(
     'pattern' => "/^\/hello\/$/",
