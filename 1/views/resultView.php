9f75738d6c36a6ccbc30dbec199da60b32fe2a7f
Kirill <kirill.herz@gmail.com>
2018-09-18 23:31:24 +0300

    Создан класс Validator

--- 1/views/resultView.php
+++ 1/views/resultView.php
@@ -2,6 +2,31 @@
 
 require_once './core/views.php';
 
+class Validator {
+
+    private $value;
+    private $isValid = true;
+
+    public function __construct($value) {
+        $this->value = $value;
+    }
+
+    public function isEmpty() {
+        $this->isValid = $this->isValid & $this->value != "";
+        return $this;
+    }
+
+    public function isNumeric() {
+        $this->isValid = $this->isValid & is_numeric($this->value);
+        return $this;
+    }
+
+    public function isValid() {
+        return $this->isValid;
+    }
+
+}
+
 class ResultView extends TwigView {
 
     function __construct() {
