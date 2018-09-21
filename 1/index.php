471150f55ab60721c782d710caca4be0212f151a
Kirill <kirill.herz@gmail.com>
2018-09-21 15:58:05 +0300

    добавлена автозагрузка классов

--- 1/index.php
+++ 1/index.php
@@ -3,10 +3,10 @@
 $uri = $_SERVER['REQUEST_URI'];
 $array = array();
 
-include_once './views/helloView.php';
-include_once './views/summaView.php';
-include_once './views/resultView.php';
-include_once './views/testDB.php';
+spl_autoload_register(function ($class_name) {
+    include "./views/" . $class_name . '.php';
+});
+
 include_once "./builders/TwigViewBuilder.php";
 
 array_push($array, array(
