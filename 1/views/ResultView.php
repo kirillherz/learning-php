b0478a7f8b887e7075afaf528c87a686a47a5e53
Kirill <kirill.herz@gmail.com>
2018-09-23 23:20:04 +0300

    cоздан класс QueryGetAll

--- 1/views/ResultView.php
+++ 1/views/ResultView.php
@@ -1,3 +1,23 @@
+<?php
+
+require_once './models/result.php';
+
+class QueryGetAll {
+
+    private $connection;
+
+    public function __construct() {
+        $this->connection = new PDO("sqlite:database.db");
+    }
+
+    public function getAll(): array {
+        $sql = 'SELECT result FROM Results';
+        $stmt = $this->connection->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_CLASS, "Result");
+    }
+
+}
+
 class ResultView extends ContextView {
 
     public function __construct() {
