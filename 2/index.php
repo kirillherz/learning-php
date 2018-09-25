3e00ebe210ffab66bb2376a8eb40cfa7a7b7112b
Kirill <kirill.herz@gmail.com>
2018-09-25 23:48:40 +0300

    добавлен пример кода phemto

--- 2/index.php
+++ 2/index.php
@@ -10,5 +10,51 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <?php
+        require_once './vendor/autoload.php';
+
+        interface IMessage {
+
+            public function getMessage();
+        }
+
+        class HelloMessage implements IMessage {
+
+            public function getMessage() {
+                return "hello";
+            }
+
+        }
+
+        interface IShow {
+
+            public function setMessage(IMessage $message);
+
+            public function show();
+        }
+
+        class Show implements IShow {
+
+            private $message;
+
+            public function setMessage(IMessage $message) {
+                $this->message = $message;
+            }
+
+            public function show() {
+                if ($this->message == NULL) {
+                    throw Exception("message is NULL");
+                } else {
+                    echo $this->message;
+                }
+            }
+
+        }
+
+        $injector = new phemto\Phemto();
+        $injector->forType("Show")->call("setMessage");
+        $show = $injector->create("Show");
+        $show->show();
+        ?>
     </body>
 </html>
